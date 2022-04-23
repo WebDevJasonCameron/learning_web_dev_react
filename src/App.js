453fe80a0f414d5957b8f87ec040698a568c7d10
Jason Cameron <dev.jasoncameron@gmail.com>
2022-04-22 21:52:01 -0500

    spinner added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import axios from "axios";
 import './App.css';
 
 class App extends Component {
+    state = {
+        users: [],
+        loading: false
+    }
+
+
     async componentDidMount() {
+        this.setState({ loading: true });
+
         const res = await axios.get('https://api.github.com/users');
 
-        console.log(res)
+        this.setState({ users: res.data, loading: false});
     }
 
     render() {
@@ -16,7 +24,7 @@ class App extends Component {
             <div className="App">
                 <Navbar />
                 <div className="container">
-                    <Users />
+                    <Users loading={this.state.loading} users={this.state.users} />
                 </div>
 
             </div>
