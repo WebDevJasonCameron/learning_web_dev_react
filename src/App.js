a2d6cb0c1672e4fac0cdbe738e69ce62c8999bb0
Jason Cameron <dev.jasoncameron@gmail.com>
2022-04-25 21:43:20 -0500

    alert added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, {Fragment, Component} from 'react';
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/search";
+import Alert from "./components/layout/alert";
 import axios from "axios";
 import './App.css';
 
 class App extends Component {
     state = {
         users: [],
-        loading: false
+        loading: false,
+        alert: null
     }
 
     // async componentDidMount() {
@@ -30,7 +32,11 @@ class App extends Component {
 
     // Clear users from state
     clearUsers = () => this.setState({ users: [], loading: false})
-
+    // Set Alert
+    setAlert = (msg, type) => {
+        this.setState({alert: {msg, type } });
+        setTimeout(() => this.setState({alert: null}), 5000)
+    }
     render() {
         const { users, loading} = this.state;
 
@@ -38,10 +44,14 @@ class App extends Component {
             <div className="App">
                 <Navbar />
                 <div className="container">
+                    <Alert alert={this.state.alert} />
                     <Search searchUsers={this.searchUsers}
                             clearUsers={this.searchUsers}
-                            showClear={users.length > 0}/>
-                    <Users loading={loading} users={users} />
+                            showClear={users.length > 0}
+                            setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users}
+                    />
                 </div>
 
             </div>
